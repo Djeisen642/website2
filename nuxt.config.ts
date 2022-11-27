a0c3058ff73bc9997385ce353cbc5e5afeb8987d
Jason <jason@medicom.us>
2022-11-26 22:15:21 -0500

    feat: add more analytics? maybe?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const authDomain =
+  process.env.NODE_ENV === 'production'
+    ? 'jasonsuttles-website.firebaseapp.com'
+    : 'jason-website-local.firebaseapp.com';
+const projectId =
+  process.env.NODE_ENV === 'production' ? 'jasonsuttles-website' : 'jason-website-local';
+const storageBucket =
+  process.env.NODE_ENV === 'production'
+    ? 'jasonsuttles-website.appspot.com'
+    : 'jason-website-local.appspot.com';
 export default defineNuxtConfig({
   css: ['@/assets/styles/index.scss'],
   debug: process.env.NODE_ENV !== 'production',
@@ -17,6 +28,9 @@ export default defineNuxtConfig({
       GOOGLE_FIREBASE_MESSAGING_ID: process.env.GOOGLE_FIREBASE_MESSAGING_ID,
       GOOGLE_FIREBASE_APP_ID: process.env.GOOGLE_FIREBASE_APP_ID,
       GOOGLE_FIREBASE_MEASUREMENT_ID: process.env.GOOGLE_FIREBASE_MEASUREMENT_ID,
+      GOOGLE_FIREBASE_AUTH_DOMAIN: authDomain,
+      GOOGLE_FIREBASE_PROJECT_ID: projectId,
+      GOOGLE_FIREBASE_STORAGE_BUCKET: storageBucket,
     },
   },
   ssr: false,
--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics, logEvent } from 'firebase/analytics';
+import { getPerformance } from '@firebase/performance';
+import { onCLS, onFID, onLCP } from 'web-vitals';
+import { ReportCallback } from 'web-vitals/src/types/base';
 import { defineNuxtPlugin, useRuntimeConfig } from '#imports';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,13 +16,44 @@ export default defineNuxtPlugin(() => {
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: runtimeConfig.GOOGLE_FIREBASE_API_KEY,
-    authDomain: 'jasonsuttles-website.firebaseapp.com',
-    projectId: 'jasonsuttles-website',
-    storageBucket: 'jasonsuttles-website.appspot.com',
+    authDomain: runtimeConfig.GOOGLE_FIREBASE_AUTH_DOMAIN,
+    projectId: runtimeConfig.GOOGLE_FIREBASE_PROJECT_ID,
+    storageBucket: runtimeConfig.GOOGLE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: runtimeConfig.GOOGLE_FIREBASE_MESSAGING_ID,
     appId: runtimeConfig.GOOGLE_FIREBASE_APP_ID,
     measurementId: runtimeConfig.GOOGLE_FIREBASE_MESSAGING_ID,
   };
   const app = initializeApp(firebaseConfig);
-  getAnalytics(app);
+  const analytics = getAnalytics(app);
+  getPerformance(app);
+
+  const sendToGoogleAnalytics: ReportCallback = ({ name, delta, id }) => {
+    // Assumes the global `gtag()` function exists, see:
+    // https://developers.google.com/analytics/devguides/collection/gtagjs
+    // https://firebase.google.com/docs/analytics/events?platform=web#log_events_5
+    logEvent(analytics, name, {
+      event_category: 'Web Vitals',
+      // The `id` value will be unique to the current page load. When sending
+      // multiple values from the same page (e.g. for CLS), Google Analytics can
+      // compute a total by grouping on this ID (note: requires `eventLabel` to
+      // be a dimension in your report).
+      event_label: id,
+      // Google Analytics metrics must be integers, so the value is rounded.
+      // For CLS the value is first multiplied by 1000 for greater precision
+      // (note: increase the multiplier for greater precision if needed).
+      value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+      // Use a non-interaction event to avoid affecting bounce rate.
+      non_interaction: true,
+
+      // OPTIONAL: any additional attribution params here.
+      // See: https://web.dev/debug-performance-in-the-field/
+      // dimension1: '...',
+      // dimension2: '...',
+      // ...
+    });
+  };
+
+  onCLS(sendToGoogleAnalytics);
+  onFID(sendToGoogleAnalytics);
+  onLCP(sendToGoogleAnalytics);
 });
