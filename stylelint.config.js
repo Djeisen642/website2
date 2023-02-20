8fa49b4380476410ccce6015dd87c3b5994c83a9
JasonS <jasonssuttles@gmail.com>
2023-02-19 23:36:01 -0500

    refactor: remove deprecated stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard-scss',
-    'stylelint-config-recommended-vue/scss',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recommended-vue/scss'],
   rules: {
-    'string-quotes': 'single',
     'color-function-notation': null, // variables in vuetify come with commas making the new rgb syntax difficult to work with
     'import-notation': 'url', // intellij doesn't understand it otherwise
   },
