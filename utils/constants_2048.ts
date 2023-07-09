e1fc1b81878d268061a04b3c5beb0712238ddf37
JasonS <jasonssuttles@gmail.com>
2023-07-08 22:24:20 -0400

    fix: use the standard 4 x 4 square

--- utils/constants_2048.ts
+++ utils/constants_2048.ts
@@ -1,3 +1,3 @@
-export const NUM_SQUARES_PER_SIDE = 5;
+export const NUM_SQUARES_PER_SIDE = 4;
 export const NUMS_THAT_CAN_BE_ADDED_AT_RANDOM = [Math.pow(2, 1), Math.pow(2, 2)];
 export const WIN_NUMBER = Math.pow(2, 11);
